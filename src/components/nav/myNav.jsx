818ddde665a8744d711224224a05e2321ba3eccb
Melkias Teshoma <melkiasapo@gmail.com>
2025-04-03 23:27:19 +0300

    non commenting

--- src/components/nav/myNav.jsx
+++ src/components/nav/myNav.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
-// import { Link as RouterLink } from "react-router-dom"; // Renaming for clarity
-// import { Link as ScrollLink } from "react-scroll";
+
 
 import "./nav.css";
 import logo from "../../assets/image/logo.png";
